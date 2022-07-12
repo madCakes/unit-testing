f3b296b9c1cad3480a737cb47b30bb2d6b3730f0
madCakes <jason.pbrooks@hotmail.com>
2022-07-12 10:47:58 +0100

    group tests together

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,11 +1,13 @@
 const getLargest = require("../getLargest");
 
-test("getLargest is a function", () => {
-  expect(typeof getLargest).toBe("function");
-});
+describe("getLargest", () => {
+  test("It is a function", () => {
+    expect(typeof getLargest).toBe("function");
+  });
 
-test("getLargest returns the largest value", () => {
-  const result = getLargest(1, 2, 3);
+  test("getLargest returns the largest value", () => {
+    const result = getLargest(1, 2, 3);
 
-  expect(result).toEqual(3);
+    expect(result).toEqual(3);
+  });
 });
